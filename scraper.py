fe7c65334950b736626a22ad286301af8083ba58
Zorin Lite <ryan.mrt1n@gmail.com>
2020-06-11 22:24:56 +0700

    fix scraper (some pages are not scraped)

--- scraper.py
+++ scraper.py
@@ -7,12 +7,8 @@ import time
 
 
 # functions
-def parse(url):
-    ''' takes url as an argument and returns a dictionary of words and their count '''
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    lyrics = [i.text for i in soup.find_all('div', class_='Lyrics__Container-sc-1ynbvzw-2 jgQsqn')]
-
+def regex(lyrics):
+    ''' regex to clean scraped lyrics '''
     lyrics = ''.join(lyrics) 
     lyrics = re.sub('\[.*?\]', '', lyrics)
     lyrics = re.sub(r'[^\w\s]', '', lyrics)
@@ -26,27 +22,45 @@ def parse(url):
             res[i] += 1
     return res
 
+def parse(url):
+    ''' takes url as an argument and returns a dictionary of words and their count '''
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    # page structure in genius.com pages are inconsistent. the lyrics are in different tags.
+    if soup.find('div', class_='Lyrics__Container-sc-1ynbvzw-2 jgQsqn') is not None: 
+        lyrics = [i.text for i in soup.find_all('div', class_='Lyrics__Container-sc-1ynbvzw-2 jgQsqn')]
+        result = regex(lyrics)
+        return result
+    elif soup.find('div', class_='Lyrics__Container-sc-1ynbvzw-2 jgQsqn') is None: 
+        if soup.find('p') is not None:
+            lyrics = soup.find('p').text
+            result = regex(lyrics)
+            return result
+        else:
+            print('unable to scrape ' + url)
+
 def append_rows():
     ''' appends words and their count from dict along with song title to a list  '''
-    for i in range(len(resp['songs'])):
-        url = resp['songs'][i]['url']
-        title = resp['songs'][i]['title']
+    for i in resp['songs']:
+        url = i['url']
+        title = i['title']
 
         words = parse(url)
-        rows = [[key, value] for key, value in words.items()]
-        for row in rows:
-            row.insert(0, title)
-            l.append(row)
+        # only append scraped data
+        if words is not None:
+            rows = [[key, value] for key, value in words.items()]
+            for row in rows:
+                row.insert(0, title)
+                l.append(row)
 
 
-# scrape
+# fake ajax request
 l = [] 
 n_page = 1
 api_url = 'https://genius.com/api/artists/1044826/songs?page='
 
 while True:
     print('sending get request to ' + api_url + str(n_page))
-    # song data is in json with key 'response'
     resp = requests.get(api_url + str(n_page)).json()['response']
 
     if resp['next_page'] is not None:
